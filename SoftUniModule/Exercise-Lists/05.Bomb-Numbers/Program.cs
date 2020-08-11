7543f3f737c2ecb425d0c279e8c814c56f471f43
Atanas Nedkov <atanas.nnedkov@gmail.com>
2020-08-11 13:00:29 +0300

    Soft-Uni-Homework

--- SoftUniModule/Exercise-Lists/05.Bomb-Numbers/Program.cs
+++ SoftUniModule/Exercise-Lists/05.Bomb-Numbers/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _05.Bomb_Numbers
 {
@@ -6,7 +8,58 @@ namespace _05.Bomb_Numbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            int[] bombInfo = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int bombNumber = bombInfo[0];
+            int bombPower = bombInfo[1];
+
+            while (true)
+            {
+                int indexOfBomb = numbers.IndexOf(bombNumber);
+                int numbersCount = numbers.Count;
+                if (indexOfBomb == -1)
+                {
+                    break;
+                }
+                int rightIndex = indexOfBomb + bombPower;
+                for (int i = indexOfBomb; i <= rightIndex; i++)
+                {
+                    if (i < numbersCount)
+                    {
+                        numbers.RemoveAt(indexOfBomb);
+
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                int leftIndex = indexOfBomb - bombPower;
+                for (int i = indexOfBomb - 1; i >= leftIndex; i--)
+                {
+                    if (i >= 0)
+                    {
+                        numbers.RemoveAt(i);
+
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            int result = numbers.Sum();
+
+          
+            Console.WriteLine(result);
+
         }
     }
 }
