20465ccad019381d80d9dfb4c53388485d7beaea
Atanas Nedkov <atanas.nnedkov@gmail.com>
2020-09-17 16:29:16 +0300

    HW

---
+++ SoftUniModule/Csharp-OOP-Interfaces-And-Abstraction-Lab/01.Shapes/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.Shapes
+{
+    class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+
+        public Rectangle(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+        
+        public void Draw()
+        {
+            Console.WriteLine(new string('*', this.Width));
+            for (int row = 1; row < this.Height - 1; row++)
+            {
+                Console.Write("*");
+                for (int i = 1; i < this.Width - 1; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("*");
+            }
+            Console.WriteLine(new string('*', this.Width));
+        }
+    }
+}
---
+++ SoftUniModule/Csharp-OOP-Interfaces-And-Abstraction-Lab/02.Cars/IElectricCar.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace _02.Cars
+{
+    public interface IElectricCar
+    {
+        public int Battery { get;}
+    }
+}
